4f521b02314e7e3da639014cc9eb102c917983b1
Emilio Peláez <me@epelaez.net>
2021-06-14 11:56:22 +0200

    Create Example Project

---
+++ Example/Shared/FocusTextFieldExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTextFieldExampleApp.swift
+//  Shared
+//
+//  Created by Emilio Peláez on 14/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FocusTextFieldExampleApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
