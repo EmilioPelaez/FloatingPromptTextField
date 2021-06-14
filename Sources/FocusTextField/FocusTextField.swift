952278f4ff86ab33147f216498145bb644ef148b
Emilio Peláez <me@epelaez.net>
2021-06-14 11:58:47 +0200

    Create FocusTextField

--- Sources/FocusTextField/FocusTextField.swift
+++ Sources/FocusTextField/FocusTextField.swift
@@ -1,6 +1,59 @@
-public struct FocusTextField {
-    public private(set) var text = "Hello, World!"
+//
+//  FocusTextField.swift
+//  FocusTextField
+//
+//  Created by Emilio Peláez on 14/6/21.
+//
 
-    public init() {
-    }
+import SwiftUI
+
+@available(iOS 15.0, *)
+struct FocusTextField<Placeholder: View>: View {
+	
+	@FocusState var isFocused: Bool
+	
+	var text: Binding<String>
+	
+	let placeholder: Placeholder
+	
+	@State private var active: Bool = false
+	
+	init(text: Binding<String>, @ViewBuilder placeholder: @escaping () -> Placeholder) {
+		self.text = text
+		self.placeholder = placeholder()
+	}
+	
+	var body: some View {
+		ZStack(alignment: .leading) {
+			TextField("", text: text)
+				.focused($isFocused)
+			placeholder
+				.scaleEffect(active ? 0.5 : 1, anchor: .leading)
+				.offset(x: 0, y: active ? -20 : 0)
+		}
+		.padding(.top, 20)
+		.onChange(of: text.wrappedValue) { _ in updateActive() }
+		.onChange(of: isFocused) { _ in updateActive() }
+	}
+	
+	func updateActive() {
+		withAnimation {
+			active = !text.wrappedValue.isEmpty || isFocused
+		}
+	}
+}
+
+@available(iOS 15.0, *)
+struct FocusTextField_Previews: PreviewProvider {
+	static var previews: some View {
+		FocusTextField(text: .constant("")) {
+			Text("Placeholder")
+		}
+		.previewLayout(.fixed(width: 200, height: 80))
+		
+		FocusTextField(text: .constant("Hello")) {
+			Text("Placeholder")
+		}
+		.previewLayout(.fixed(width: 200, height: 80))
+	}
 }
